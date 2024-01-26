5f1e70f30e70eb3acf580c31f13215ef5854b829
zeinab <swarsama@hotmail.co.uk>
2024-01-26 18:34:29 +0000

    JS now working - checking for a valid email, taking to a different interface when correct email submitted and confirming the email submitted

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
-// console.log("hello");
-
+const form = document.querySelector(".form");
 const input = document.querySelector("#email");
 const btn = document.querySelector(".submit-btn");
 const errorText = document.querySelector(".hidden");
 const confirmation = document.querySelector(".confirmation-hidden");
 const emailContainer = document.querySelector(".text-container");
+const emailConfirmed = document.querySelector("span");
 
-input.addEventListener("submit", (event) => {
-  console.log(event);
+function valid(email) {
+  const pattern = /^[^@\s]+@[^@\s]+$/;
+  return pattern.test(email);
+} ;
 
-});
 
-btn.addEventListener("click", (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("clicked")
+  // console.log("submit")
+  // console.log(input.value);
+  if (!valid(input.value)) {
+    errorText.classList.remove("hidden");
+  } else {
   confirmation.classList.remove("confirmation-hidden");
   emailContainer.style.display = "none";
+  emailConfirmed.innerText =input.value ;
+  }
+  end
 });
+
+
+
+
+
+
+
+
+
+//   emailContainer.style.display = "none";
+  // console.log(event);
+  // console.log(event.innerText);
+
+  // console.log("enter");
+  // if (input.value === (valid))
+  //   errorText.classList.remove("hidden");
+  // end
+  // errorText.classList.remove("hidden");
+// btn.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   console.log("clicked")
+//   confirmation.classList.remove("confirmation-hidden");
+//   emailContainer.style.display = "none";
+// });
